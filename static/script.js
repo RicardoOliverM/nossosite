066ed05b8f2c8bddb74004f46ec0f479769fd389
Ricardo R <53445463+RicardoOliverM@users.noreply.github.com>
2025-03-16 17:29:23 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,13 @@
-// Apenas o código de animação dos confetes permanece no script.js
-//function estourarConfete() {
-  //  setTimeout(() => {
-    //    confetti({
-      //      particleCount: 150,
-        //    spread: 120,
-          //  origin: { y: 0.7 }
-        //});
-    //}, 500);
-//}
+
+function estourarConfete() {
+    setTimeout(() => {
+      confetti({
+           particleCount: 150,
+            spread: 120,
+            origin: { y: 0.7 }
+        });
+    }, 500);
+}
 function verificarData() {
     let hoje = new Date();
     let dia = hoje.getDate();
