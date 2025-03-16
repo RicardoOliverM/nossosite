a2952ac7a13fb66d371df9ab8cb51497e8dd2a14
Ricardo R <53445463+RicardoOliverM@users.noreply.github.com>
2025-03-16 17:28:57 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,20 @@
 // Apenas o código de animação dos confetes permanece no script.js
-function estourarConfete() {
-    setTimeout(() => {
-        confetti({
-            particleCount: 150,
-            spread: 120,
-            origin: { y: 0.7 }
-        });
-    }, 500);
+//function estourarConfete() {
+  //  setTimeout(() => {
+    //    confetti({
+      //      particleCount: 150,
+        //    spread: 120,
+          //  origin: { y: 0.7 }
+        //});
+    //}, 500);
+//}
+function verificarData() {
+    let hoje = new Date();
+    let dia = hoje.getDate();
+    
+    // Forçar a condição de "dia 9"
+    if (dia === 9 || true) {  // A condição `true` faz com que os confetes sempre estoure
+        estourarConfete();
+    }
 }
+
